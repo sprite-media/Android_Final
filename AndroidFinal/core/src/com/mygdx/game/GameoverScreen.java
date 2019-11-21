1b2563f04c9716c1cbb9f18efd85a536d8250a11
Jin <bomjan@naver.com>
2019-11-21 15:06:39 -0500

    temp

---
+++ AndroidFinal/core/src/com/mygdx/game/GameoverScreen.java
@@ -0,0 +1,25 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public class GameoverScreen extends ScreenBeta
+{
+	Skin orange;
+	Table uiTable;
+
+
+    @Override
+    public void initialize()
+    {
+		orange = new Skin(Gdx.files.internal("Skin/orange/skin/uiskin.json"));
+		uiTable = new Table(orange);
+    }
+
+    @Override
+    public void update(float dt)
+    {
+
+    }
+}

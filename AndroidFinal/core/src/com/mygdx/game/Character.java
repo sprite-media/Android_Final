bc7068bcfda959a727cb97e839b83ccbaadad4df
Heaseo Chung <look0713@hotmail.com>
2019-11-23 17:50:14 -0500

    add function in power ups class

--- AndroidFinal/core/src/com/mygdx/game/Character.java
+++ AndroidFinal/core/src/com/mygdx/game/Character.java
@@ -131,7 +131,7 @@ public class Character extends ActorBeta
 
     public void GetPowerUp(int _ups)
     {
-        life++;
+        life += _ups;
         ChangeAnim();
     }
 
--- AndroidFinal/core/src/com/mygdx/game/PowerUp.java
+++ AndroidFinal/core/src/com/mygdx/game/PowerUp.java
@@ -1,4 +1,62 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+import java.util.Random;
+
 public class PowerUp extends ActorBeta {
+
+    Vector2 size;
+
+    //Player
+    Character player;
+
+    //Position
+    Random randPosX;
+    Random randPosY;
+
+    int powerUp;
+
+
+    PowerUp(float x, float y, Stage s, Character _player)
+    {
+        loadTexture("Character/Powerup.png");
+
+        player = _player;
+        //Set Position
+        setPosition(randPosX.nextInt(Gdx.graphics.getWidth()), randPosY.nextInt(Gdx.graphics.getHeight()) + Gdx.graphics.getHeight()/2);
+
+        //Set Size
+        size = new Vector2(6, 6);
+        size.x = Gdx.graphics.getWidth()/size.x;
+        size.y = Gdx.graphics.getWidth()/size.y;
+        setSize(size.x, size.y);
+    }
+
+
+    @Override
+    public void act(float dt)
+    {
+        super.act(dt);
+
+        if(this == null)
+            return;
+
+        if(getY() <= 0)
+        {
+            this.remove();
+        }
+
+        //TODO Chagne position of this obj Y here
+
+        if(this.overlaps(player))
+        {
+            Random random = new Random();
+            powerUp = random.nextInt(10) + 1;
+            player.GetPowerUp(powerUp);
+            this.remove();
+        }
+    }
 }
